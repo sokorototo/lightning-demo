ac51dfbd22917b7bc43432a80513d4436ff92d51
sokorototo <nyachiengatoto@gmail.com>
2021-07-29 17:05:59 +0300

    MM

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
 "use strict";
+
 const canvas = document.getElementById("cx");
+
 const engine = new BABYLON.Engine(canvas, true, { alpha: true });
+engine.setHardwareScalingLevel(2);
+
 const scene = new BABYLON.Scene(engine);
+scene.debugLayer.show();
+scene.clearColor = BABYLON.Color3.Teal();
+
+const reflectionTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("src/environment.env", scene);
+scene.environmentTexture = reflectionTexture;
+
 const camera = new BABYLON.ArcRotateCamera('main_camera', 0, 0, 10, BABYLON.Vector3.Zero(), scene);
-const main_light = new BABYLON.PointLight('main_light', BABYLON.Vector3.Up().scale(3), scene);
-const floor = BABYLON.MeshBuilder.CreateBox('floor', {}, scene);
+camera.minZ = 0;
+camera.attachControl(canvas, true);
+
+const floor = BABYLON.MeshBuilder.CreateBox('floor', {
+	depth: 200,
+	height: 0.1,
+	width: 200
+}, scene);
+floor.material = new BABYLON.PBRMaterial('floor_mat', scene);
+
+const assetContainer = new BABYLON.AssetContainer(scene);
+
 window.addEventListener('resize', () => {
-    engine.resize();
+	engine.resize();
 });
+
 engine.runRenderLoop(() => {
-    scene.render();
+	scene.render();
 });
+
+console.log(window);
