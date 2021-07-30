1bb83bdd02374708fe5df87423cd8aade91e254b
sokorototo <nyachiengatoto@gmail.com>
2021-07-30 04:06:45 +0300

    Working but shit import macros

--- src/loader.js
+++ src/loader.js
@@ -1,26 +1,60 @@
 "use strict"
-class TypeError extends Error{
-	constructor(message, type){
-		this.type = type;
+class TypeError extends Error {
+	constructor(message, type) {
 		super(message);
+		this.type = type;
 	}
 }
 
-export default async function load(macroUrl) { // TODO: Import BABYLON object instead of expecting it to be available globally
-	const sep = '/'; // To avoid redeclaration
-	if (!macroUrl.endsWith(sep)) macroUrl += sep;
-	const {globals, ...props} = await fetch(`${macroUrl}import.json`).then(res => res.json());
+function parse(_type, _props, _globals, _scene, _home) {
+	for (let i = 0; i < +_globals?.length; i++) {
+		// Synchronize globals
+		const global = _globals[i];
 
-	switch (props.type) {
+		for (const key in _props) {
+			if (key === 'name') continue;
+
+			const property = _props[key];
+			if (property.type === global.type) Object.assign(property, global);
+		};
+	};
+
+	switch (_type) {
 		case "PBRMaterial":
-			console.log('[LOADER] Loading PBRMaterial!');
-			for (let i = 0; i < globals.length; i++) {
-				const global = globals[i];
-				console.log(global);
-			}
-			break;
-	
+
+			const mat = new BABYLON.PBRMaterial(_props["name"] || "pbr_material", _scene);
+			for (const key in _props) {
+				if (key === 'name') continue;
+
+				const property = _props[key];
+				const { type, globals, ...props } = property;
+				mat[key] = parse(type, props, globals, _scene, _home);
+			};
+			return mat;
+
+		case "Flag":
+			return _props.value
+
+		case "Texture":
+			const texture = new BABYLON.Texture(`${_home}${_props['url']}`, _scene);
+			for (const key in _props) {
+				if (key === 'name' || key === 'url') continue;
+				console.log(key);
+
+				const property = _props[key];
+				const { type, globals, ...props } = property;
+				texture[key] = parse(type, props, globals, _scene, _home);
+			};
+			return texture
 		default:
-			throw new TypeError('Unknown import type found', props.type);
+			return new TypeError('Unknown import type found', _type);
 	}
 }
+
+export default async function load(macroUrl, scene) { // TODO: Import BABYLON object instead of expecting it to be available globally
+	const sep = '/'; // To avoid re-declarations
+	if (!macroUrl.endsWith(sep)) macroUrl += sep;
+	const { type, globals, ...props } = await fetch(`${macroUrl}import.json`).then(res => res.json());
+
+	return parse(type, props, globals, scene, macroUrl);
+};
--- src/main.js
+++ src/main.js
@@ -1,36 +1,55 @@
 "use strict";
+import Loader from './loader.js';
 
-const canvas = document.getElementById("cx");
+async function init(ev) {
+	const canvas = document.getElementById("cx");
 
-const engine = new BABYLON.Engine(canvas, true, { alpha: true });
-engine.setHardwareScalingLevel(2);
+	const engine = new BABYLON.Engine(canvas, true, { alpha: true });
+	engine.setHardwareScalingLevel(2);
 
-const scene = new BABYLON.Scene(engine);
-scene.debugLayer.show();
-scene.clearColor = BABYLON.Color3.Teal();
+	const scene = new BABYLON.Scene(engine);
+	scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
+	scene.clearColor = BABYLON.Color3.FromHexString("#002828");
+	scene.debugLayer.show();
+	Loader('src/floor_mat', scene).then(console.log)
 
-const reflectionTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("src/environment.env", scene);
-scene.environmentTexture = reflectionTexture;
+	// scene.onPointerDown = () => { if (!engine.isPointerLock) engine.enterPointerlock() };
 
-const camera = new BABYLON.ArcRotateCamera('main_camera', 0, 0, 10, BABYLON.Vector3.Zero(), scene);
-camera.minZ = 0;
-camera.attachControl(canvas, true);
+	const reflectionTexture = BABYLON.CubeTexture.CreateFromPrefilteredData("src/environment.env", scene);
+	scene.environmentTexture = reflectionTexture;
+	scene.environmentIntensity = 0.35;
 
-const floor = BABYLON.MeshBuilder.CreateBox('floor', {
-	depth: 200,
-	height: 0.1,
-	width: 200
-}, scene);
-floor.material = new BABYLON.PBRMaterial('floor_mat', scene);
+	const camera = new BABYLON.FreeCamera('main_camera', BABYLON.Vector3.Up().scale(12), scene);
+	camera.attachControl(canvas, true);
+	camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+	camera.checkCollisions = true;
+	camera.applyGravity = true;
+	camera.position.y += 12;
+	camera.minZ = 0;
 
-const assetContainer = new BABYLON.AssetContainer(scene);
+	const floor = BABYLON.MeshBuilder.CreateGround('floor', {
+		height: 200,
+		width: 200
+	}, scene);
+	floor.checkCollisions = true;
 
-window.addEventListener('resize', () => {
-	engine.resize();
-});
+	floor.material = new BABYLON.PBRMaterial('floor_mat', scene);
+	floor.material.backFaceCulling = false;
+	floor.material.albedoColor = BABYLON.Color3.FromHexString("#3E1E00");
+	floor.material.roughness = 0.7;
+	floor.material.metallic = 0.6;
 
-engine.runRenderLoop(() => {
-	scene.render();
-});
+	const assetContainer = BABYLON.SceneLoader.LoadAssetContainer('./src/gun/', 'main.babylon', scene, (container) => {
+		container.addAllToScene();
+	});
 
-console.log(window);
+	window.addEventListener('resize', () => {
+		engine.resize();
+	});
+
+	engine.runRenderLoop(() => {
+		scene.render();
+	});
+};
+
+window.addEventListener('load', init, { once: true , passive: true});
